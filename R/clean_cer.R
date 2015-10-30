af59e45ec7cb301f6b75d258371a152c85ab98c0
Danton Noriega <danton.noriega@gmail.com>
2015-10-30 11:08:05 -0400

    finished kwh spike smoothing function. redesigning weekend/weekday code

--- R/clean_cer.R
+++ R/clean_cer.R
@@ -2,7 +2,8 @@
 #' the raw data using get_cer(). It will first fill in any
 #'
 #' @param DT_KWH (data.table) table with CER consumption data
-balance_kwh <- function(DT_KWH) {
+#' @param drop_dst (logical) drop Daylight savings observations (data will NOT be perfectly balance if FALSE)
+balance_kwh <- function(DT_KWH, drop_dst=FALSE) {
 
   message('balancing data...')
   # modal value of 30 min interval counts
@@ -26,35 +27,33 @@ balance_kwh <- function(DT_KWH) {
   # 5 day cut off
   keep_ids <- unique(DT_na[days<=5]$id)
   drop_ids <- unique(DT_na[days>5]$id)
-  DT2 <- DT_bal[!id %in% drop_ids]
+  DT1 <- DT_bal[!id %in% drop_ids]
   message(length(drop_ids), ' ids removed.')
-  return(DT2)
+  return(DT1)
 
 }
 
-#' clean residential kwh data.
-clean_residential_kwh <- function(DT_KWH) {
-
-  CORES <- detectCores() - 1
+#' imput residential kwh data. imputation can take a LONG time.
+imput_residential_kwh <- function(DT_KWH) {
 
   # check to see if residential only data
   if(!"tar_stim" %in% names(DT_KWH)) {
     DT1 <- merge(DT_KWH, cer_assign, by = "id")
   }
   # balance data
-  DT2 <- balance_kwh(DT1)[, .(id, date_cer, kw, kwh)]
-  setkey(DT2, id, date_cer)
+  DT1 <- balance_kwh(DT1)[, .(id, date_cer, kw, kwh)]
+  setkey(DT1, id, date_cer)
   weekdays <- cer_ts[weekday>0, .(date_cer, weekday)]
   weekends <- cer_ts[weekday<1, .(date_cer, weekday)]
 
-  # imput weekdays
+# IMPUT WEEKDAYS ----------------------------------------------------------
   ## data table to search
-  DT_wkdy <- merge(DT2, weekdays, by = 'date_cer')
+  DT_wkdy <- merge(DT1, weekdays, by = 'date_cer') # weekday data
   DT_wkdy[, hour_cer := date_cer %%100]
   DT_wkdy[, day_cer  := (date_cer - hour_cer)/100]
-  setkey(DT_wkdy, id, date_cer)
 
   ## create weekday indx
+  setkey(DT_wkdy, id, date_cer) # crucial that we sequentially
   ids <- unique(DT_wkdy$id)
   dates <- unique(DT_wkdy$date_cer)
   days <- unique((dates - dates%%100)/100)
@@ -64,65 +63,115 @@ clean_residential_kwh <- function(DT_KWH) {
   indx <- which(is.na(NT), arr.ind = TRUE) # find r,c index of missing vals
   indx <- as.data.table(indx)
   indx[, id := ids[col]]
-  indx[, date_cer := dates[row]]
+  indx[, date_cer := dates[row]] # only works if data is sequential!
   indx[, hour_cer := date_cer %%100]
   indx[, day_cer  := (date_cer - hour_cer)/100]
+  # set keys for quick search and column binding
   setkey(DT_wkdy, id, hour_cer, day_cer)
   setkey(indx, id, hour_cer, day_cer)
-  imputs <- mapply(function(x, y, z) {
-    j <- which(days %in% z) # match to cer_days vector
-    past_days <- days[(j-1):(j-10)] # sequence back 5 weekdays
-    past_kwh <- vapply(past_days, function(i) DT_wkdy[.(x,y,i)]$kwh, numeric(1))
-    avg10 <- mean(past_kwh, na.rm=TRUE) # avg of past 10 readings at given hour
-    DT_wkdy[.(x,y,z), kwh:=avg10]
-  }, x=indx$id, y=indx$hour_cer, z=indx$day_cer)
+  # parallel search
+  start <- proc.time()
+  if(require(parallel) & .Platform$OS.type == "unix") {
+    message('parallel package found on Mac/Linux system. using mcmapply.')
+    CORES <- detectCores() - 1
+    imputs <- mcmapply(function(x, y, z) { # use mcmapply
+      j <- which(days %in% z) # match to cer_days vector
+      past_days <- days[(j-1):(j-10)] # sequence back 5 weekdays
+      past_kwh <- vapply(past_days, function(i) DT_wkdy[.(x,y,i)]$kwh, numeric(1))
+      avg10 <- mean(past_kwh, na.rm=TRUE) # avg of past 10 readings at given hour
+      return(avg10)
+    }, x=indx$id, y=indx$hour_cer, z=indx$day_cer, mc.cores=CORES)
+  } else {
+    message('parallel NOT package found. NOT using mcmapply.')
+    imputs <- mapply(function(x, y, z) { # use mcmapply
+      r <- which(days %in% z) # match to cer_days vector
+      past_days <- days[(j-1):(j-10)] # sequence back 10 weekdays
+      past_kwh <- vapply(past_days, function(i) DT_wkdy[.(x,y,i)]$kwh, numeric(1))
+      avg10 <- mean(past_kwh, na.rm=TRUE) # avg of past 10 readings at given hour
+      return(avg10)
+    }, x=indx$id, y=indx$hour_cer, z=indx$day_cer)
+  }
+  end <- proc.time() - start
+  DT_wkdy[is.na(kwh), kwh:=imputs]
+  DT_wkdy[is.na(kw), kw:=kwh*2]
 
+# IMPUT WEEKENDS ----------------------------------------------------------
+  ## data table to search
+  DT_wknd <- merge(DT1, weekends, by = 'date_cer') # weekend data
+  DT_wknd[, hour_cer := date_cer %%100]
+  DT_wknd[, day_cer  := (date_cer - hour_cer)/100]
 
+  ## create weekday indx
+  setkey(DT_wknd, id, date_cer) # crucial that we sequentially
+  ids <- unique(DT_wknd$id)
+  dates <- unique(DT_wknd$date_cer)
+  days <- unique((dates - dates%%100)/100)
+  N <- length(ids)
+  T <- length(dates)
+  NT <- matrix(DT_wknd$kwh,nrow=T,ncol=N) # rows = T, cols = N
+  indx <- which(is.na(NT), arr.ind = TRUE) # find r,c index of missing vals
+  indx <- as.data.table(indx)
+  indx[, id := ids[col]]
+  indx[, date_cer := dates[row]] # only works if data is sequential!
+  indx[, hour_cer := date_cer %%100]
+  indx[, day_cer  := (date_cer - hour_cer)/100]
+  # set keys for quick search and column binding
+  setkey(DT_wknd, id, hour_cer, day_cer)
+  setkey(indx, id, hour_cer, day_cer)
+  # parallel search
+  start <- proc.time()
+  if(require(parallel) & .Platform$OS.type == "unix") {
+    message('parallel package found on Mac/Linux system. using mcmapply.')
+    CORES <- detectCores() - 1
+    imputs <- mcmapply(function(x, y, z) { # use mcmapply
+      j <- which(days %in% z) # match to cer_days vector
+      past_days <- days[(j-1):(j-10)] # sequence back 5 weekdays
+      past_kwh <- vapply(past_days, function(i) DT_wknd[.(x,y,i)]$kwh, numeric(1))
+      avg10 <- mean(past_kwh, na.rm=TRUE) # avg of past 10 readings at given hour
+      return(avg10)
+    }, x=indx$id, y=indx$hour_cer, z=indx$day_cer, mc.cores=CORES)
+  } else {
+    message('parallel NOT package found. NOT using mcmapply.')
+    imputs <- mapply(function(x, y, z) { # use mcmapply
+      r <- which(days %in% z) # match to cer_days vector
+      past_days <- days[(j-1):(j-10)] # sequence back 10 weekdays
+      past_kwh <- vapply(past_days, function(i) DT_wknd[.(x,y,i)]$kwh, numeric(1))
+      avg10 <- mean(past_kwh, na.rm=TRUE) # avg of past 10 readings at given hour
+      return(avg10)
+    }, x=indx$id, y=indx$hour_cer, z=indx$day_cer)
+  }
+  end <- proc.time() - start
+  DT_wknd[is.na(kwh), kwh:=imputs]
+  DT_wknd[is.na(kw), kw:=kwh*2]
 
+# END IMPUT ------------------------------------------------------------
+  DT1 <- rbindlist(list(DT_wknd, DT_wkdy))
+  setkey(DT1, id, date_cer)
+  return(DT1)
 }
 
-#' find zeros or missing kwh data.
-imput_kwh_nas <- function(DT_KWH) {
+#' find surges in kwh and smooth them out. Pon (2015) finds that average KWH consumption per household is about 11-12 kwh. It is assumed that a surge, therefore, would be anything about 6 kwh per half hour (12 kwh).
+#' @param kwh_surge set value of what is considered a kwh surge
+smooth_residential_kwh <- function(DT_KWH, kwh_surge = 6) {
 
   # check to see if residential only data
   if(!"tar_stim" %in% names(DT_KWH)) {
     DT1 <- merge(DT_KWH, cer_assign, by = "id")
   }
-
-  # find index of zeros
+  # find index of high values
   setkey(DT1, id, date_cer)
-  zs  <- DT1[, .(indx=which(kwh==0))]
-  zs1 <- zs[, .(indx=indx+1)]
-  zs_1<- zs[, .(indx=indx-1)]
-  Z <- unique(rbindlist(list(zs, zs1, zs_1)))
-  setkey(Z, indx)
-  Z <- Z$indx
-  h <- hist(DT1[Z][kwh>0]$kwh) # get histogram object of nonzero values
-  nms <- paste(h$breaks[-length(h$breaks)], h$breaks[-1], sep="-")
-  counts <- h$counts
-  names(counts) <- nms
-}
+  s <- DT1[, .(indx=which(kwh>kwh_surge))]
+  f <- s[, .(indx=indx+1)]
+  b <- s[, .(indx=indx-1)]
+  diff1 <- DT1[s$indx]$kwh - DT1[b$indx]$kwh > 1/2*kwh_surge # sudden surge
+  diff2 <- DT1[s$indx]$kwh - DT1[f$indx]$kwh > 1/2*kwh_surge # sudden drop
+  diff <- diff1 & diff2
+  avg <- (DT1[b[diff]$indx]$kwh + DT1[f[diff]$indx]$kwh)/2
+  smooth <- s[diff]$indx
+  message(length(smooth), ' surge values found and corrected.')
+  DT1[smooth, kwh:=avg]
+  return(DT1)
 
-#' find zeros or missing kwh data.
-find_zeros_or_nas <- function(DT_KWH) {
-
-  # check to see if residential only data
-  if(!"tar_stim" %in% names(DT_KWH)) {
-    DT1 <- merge(DT_KWH, cer_assign, by = "id")
-  }
-
-  # find index of zeros
-  setkey(DT1, id, date_cer)
-  zs  <- DT1[, .(indx=which(kwh==0))]
-  zs1 <- zs[, .(indx=indx+1)]
-  zs_1<- zs[, .(indx=indx-1)]
-  Z <- unique(rbindlist(list(zs, zs1, zs_1)))
-  setkey(Z, indx)
-  Z <- Z$indx
-  h <- hist(DT1[Z][kwh>0]$kwh) # get histogram object of nonzero values
-  nms <- paste(h$breaks[-length(h$breaks)], h$breaks[-1], sep="-")
-  counts <- h$counts
-  names(counts) <- nms
 }
 
 
